f304b0e40b2c2b4ad012ffa64252f275c9249327
Suhas Krishna Srivastava <suhasCanDoSo@gmail.com>
2025-04-11 20:12:16 +0530

    error: renamed errors.
    errors not native to `enc` anymore.

--- src/vt/utils/errors/error_specs/error_codes.py
+++ src/vt/utils/errors/error_specs/error_codes.py
@@ -6,53 +6,53 @@ Standard POSIX error codes taken from: https://tldp.org/LDP/abs/html/exitcodes.h
 Also taken from sysexit.h
 """
 
-ENC_EXIT_OK = 0
+EXIT_OK = 0
 "Everything is okay"
 
-ENC_ERR_GENERIC_ERR = 1
+ERR_GENERIC_ERR = 1
 "Some generic error"
 
-ENC_ERR_INVALID_USAGE = 2
+ERR_INVALID_USAGE = 2
 "Invalid usage command"
 
-ENC_ERR_STATE_ALREADY_EXISTS = 4
+ERR_STATE_ALREADY_EXISTS = 4
 "State already exists"
 
-ENC_FILE_ALREADY_EXISTS = ENC_ERR_STATE_ALREADY_EXISTS
+FILE_ALREADY_EXISTS = ERR_STATE_ALREADY_EXISTS
 "File already exists"
 
-ENC_DIR_ALREADY_EXISTS = ENC_ERR_STATE_ALREADY_EXISTS
+DIR_ALREADY_EXISTS = ERR_STATE_ALREADY_EXISTS
 "Directory already exists"
 
-ENC_ERR_DATA_FORMAT_ERR = 65  # EX_DATAERR in sysexits.h
+ERR_DATA_FORMAT_ERR = 65  # EX_DATAERR in sysexits.h
 "Data format error, for example, while reading from a config file"
 
-ENC_UNAVAILABLE_SERVICE_ERR = 69  # EX_UNAVAILABLE in sysexit.h
+UNAVAILABLE_SERVICE_ERR = 69  # EX_UNAVAILABLE in sysexit.h
 "Service unavailable"
 
-ENC_UNSTABLE_STATE_ERR = ENC_UNAVAILABLE_SERVICE_ERR  # EX_UNAVAILABLE in sysexit.h
+UNSTABLE_STATE_ERR = UNAVAILABLE_SERVICE_ERR  # EX_UNAVAILABLE in sysexit.h
 "Service unavailable"
 
-ENC_UNINITIALISED_ERR = ENC_UNAVAILABLE_SERVICE_ERR  # EX_UNAVAILABLE in sysexit.h
+UNINITIALISED_ERR = UNAVAILABLE_SERVICE_ERR  # EX_UNAVAILABLE in sysexit.h
 "Service unavailable"
 
-ENC_CANNOT_EXECUTE_CMD = 126
+CANNOT_EXECUTE_CMD = 126
 "Command cannot be executed"
 
-ENC_CMD_EXECUTION_PERMISSION_DENIED = 126
+CMD_EXECUTION_PERMISSION_DENIED = 126
 "Operation unauthorized"
 
-ENC_ERR_CMD_NOT_FOUND = 127
+ERR_CMD_NOT_FOUND = 127
 "Command not found"
 
-ENC_ERR_FILE_NOT_FOUND = ENC_ERR_CMD_NOT_FOUND
+ERR_FILE_NOT_FOUND = ERR_CMD_NOT_FOUND
 "File not found"
 
-ENC_ERR_DIR_NOT_FOUND = ENC_ERR_CMD_NOT_FOUND
+ERR_DIR_NOT_FOUND = ERR_CMD_NOT_FOUND
 "Directory not found"
 
-ENC_ERR_UNDERLYING_CMD_ERR = 128
+ERR_UNDERLYING_CMD_ERR = 128
 "Underlying command execution error"
 
-ENC_ERR_SIGINT_RECEIVED = 130  # Ctrl-C
+ERR_SIGINT_RECEIVED = 130  # Ctrl-C
 "Interrupt signal received"
