cbbb4bb240178da8f373898103b1fd1bb5e6cf40
Suhas Krishna Srivastava <suhasCanDoSo@gmail.com>
2025-04-12 21:58:38 +0530

    error: ``not_allowed_together`` method added.

--- src/vt/utils/errors/error_specs/utils.py
+++ src/vt/utils/errors/error_specs/utils.py
@@ -6,7 +6,11 @@ Utility methods for error specifications.
 """
 
 
+import warnings
 from typing import Any
+import inspect
+
+from vt.utils.errors.warnings import vt_warn
 
 
 def form_errmsg_for_choices(emphasis: str | None = None, choices: list[Any] | None = None) -> str:
@@ -40,3 +44,92 @@ def form_errmsg_for_choices(emphasis: str | None = None, choices: list[Any] | No
     if choices:
         errmsg = f"{errmsg} Choose from {choices}."
     return errmsg
+
+
+def not_allowed_together(*args: Any | None, __err: bool = True, __truthy: bool = False,
+                         __fmt: str = "{category}: {message}\n",
+                         **name_overrides: str | None):
+    """
+    Raise an error or warning if all provided positional arguments are not ``None`` or falsy.
+
+    This utility is designed to ensure that a group of arguments *cannot* be used together.
+    If all of them are provided (i.e., __truthy) or not ``None``, a ``ValueError`` or ``UserWarning`` is raised/emitted.
+
+    :param args: The arguments to check. Only truthy arguments are considered "provided" if ``__truthy`` is ``True``
+        else ``non-None`` areguments are considered "provided".
+    :param __truthy: Check for arguments being truthy instead of ``None``.
+    :param __err: If ``True``, raise a ``ValueError`` when all arguments are truthy (or not ``None``).
+        If ``False``, emits a ``UserWarning`` instead.
+    :param __fmt: format in which the warning will be raised. See ``vt_warn()`` for more details.
+        Only relevant if ``__err`` is ``False``.
+    :param name_overrides: Optional keyword arguments to rename argument labels in the error message.
+            Provide the argument as keyword with the original name and new string name as value. e.g.
+            ``not_allowed_together(a, b, b="no_b")`` will show "'a' and 'no_b'" in the message.
+
+    :raise UserWarning: If all provided arguments are truthy (or not ``None``) and ``__err`` is ``False``.
+    :raise ValueError: If all provided arguments are truthy (or not ``None``) and ``__err`` is ``True``.
+
+    Examples
+    --------
+    >>> import contextlib
+    >>> import sys
+    >>> a = 1
+    >>> b = 2
+    >>> not_allowed_together(a, b)
+    Traceback (most recent call last):
+    ...
+    ValueError: 'a' and 'b' are not allowed together
+
+    >>> with warnings.catch_warnings():
+    ...     with contextlib.redirect_stderr(sys.stdout):
+    ...         not_allowed_together(a, b, b="param_b", __err=False)
+    UserWarning: 'a' and 'param_b' are not allowed together
+
+    >>> not_allowed_together(None, b) # No error
+
+    >>> not_allowed_together(0, None) # No error
+
+    >>> a = "A"
+    >>> b = "B"
+    >>> c = "C"
+    >>> not_allowed_together(a, b, c)
+    Traceback (most recent call last):
+    ...
+    ValueError: 'a', 'b', and 'c' are not allowed together
+
+    >>> not_allowed_together(a, b, c, a="X", c="Z")
+    Traceback (most recent call last):
+    ...
+    ValueError: 'X', 'b', and 'Z' are not allowed together
+
+    >>> b = {}
+    >>> not_allowed_together(a, {}, c, __truthy=True) # no error as not all args are truthy ({}, 2nd arg)
+
+    >>> not_allowed_together(a, b, c) # err as b is not None and __truthy is defaulted to False.
+    Traceback (most recent call last):
+    ...
+    ValueError: 'a', 'b', and 'c' are not allowed together
+    """
+
+    # Get argument names (best-effort using stack)
+    frame = inspect.currentframe().f_back
+    arg_names = [name for name, val in frame.f_locals.items() if val in args]
+
+    # Use name overrides if provided
+    for i, name in enumerate(arg_names):
+        if name in name_overrides:
+            arg_names[i] = name_overrides[name]
+
+    valid = all(args) if __truthy else all(arg is not None for arg in args)
+    if valid:
+        # Format names nicely
+        if len(arg_names) > 2:
+            formatted = "', '".join(arg_names[:-1])
+            msg = f"'{formatted}', and '{arg_names[-1]}' are not allowed together"
+        else:
+            msg = f"'{arg_names[0]}' and '{arg_names[1]}' are not allowed together"
+
+        if __err:
+            raise ValueError(msg)
+        else:
+            vt_warn(msg, fmt=__fmt, stack_level=3)
