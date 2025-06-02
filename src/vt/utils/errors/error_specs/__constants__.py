31b98d2fc4b6d2776688d7d00090258dba04143c
Suhas Krishna Srivastava <suhasCanDoSo@gmail.com>
2025-06-02 14:00:48 +0530

    error: added more type consts.

--- src/vt/utils/errors/error_specs/__constants__.py
+++ src/vt/utils/errors/error_specs/__constants__.py
@@ -7,9 +7,12 @@ Constants related to errors.
 
 from vt.utils.errors.error_specs.error_codes import *
 
+import pathlib as __pathlib
+
 type_name_map: dict[type, str] = {
     str: 'a string',
     int: 'an int',
     float: 'a float',
-    bool: 'a boolean'
+    bool: 'a boolean',
+    __pathlib.Path: 'a Path'
 }
